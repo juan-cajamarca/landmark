8fbcbcb0220846519ad3cf7d1e0d2b86088767e3
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-20 02:45:57 -0500

    4.5 Adding navigation between sections

--- Landmark/Supporting Views/LandmarkList.swift
+++ Landmark/Supporting Views/LandmarkList.swift
@@ -11,28 +11,28 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
-            List {
-                Toggle(isOn: $userData.showFavoritesOnly) {
-                    Text("Favorites only")
-                }
-                
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Favorites only")
+            }
+            
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationBarTitle("Landmarks")
         }
+        .navigationBarTitle("Landmarks")
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-            .environmentObject(UserData())
+        NavigationView {
+            LandmarkList()
+                .environmentObject(UserData())
         }
+    }
 }
