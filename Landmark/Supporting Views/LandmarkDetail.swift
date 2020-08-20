f8f5bff9fbe8f6b053b62febaec394c127f09b02
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-20 01:43:21 -0500

    3.6 Creating a Favorite Button for Each Landmark

--- Landmark/Supporting Views/LandmarkDetail.swift
+++ Landmark/Supporting Views/LandmarkDetail.swift
@@ -26,8 +26,22 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.landmarks[self.landamrkIndex].isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[landamrkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
