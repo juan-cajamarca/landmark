6dfedeb7d747dca3e880f0099542be21b6d7ffa8
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-24 12:37:45 -0500

    5.1 Creating a Badge View

---
+++ Landmark/Supporting Views/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  Landmark
+//
+//  Created by Juan David Cajamarca Acuña on 24/08/20.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
