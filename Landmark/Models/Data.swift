6f02fdbcafff15de8ac5a09bbf6284f4b53e4c13
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-25 11:27:29 -0500

    6.1 Adding animations to individual views

--- Landmark/Models/Data.swift
+++ Landmark/Models/Data.swift
@@ -15,6 +15,7 @@ import SwiftUI
 import CoreLocation
 
 let landmarkData: [LandmarkModel] = load("landmarkData.json")
+let hikeData: [Hike] = load("hikeData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
