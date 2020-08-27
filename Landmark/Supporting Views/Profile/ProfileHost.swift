d14e2b138b20b67b9be2ff5b35e3db04e0e51cd6
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-27 00:27:49 -0500

    7.1 Displaying a User Profile

---
+++ Landmark/Supporting Views/Profile/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmark
+//
+//  Created by Juan David Cajamarca Acuña on 26/08/20.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
